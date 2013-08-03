8ab3ea1c7602eaeb72d61b1485251cf3edb1ac8d
villfa <fabien.villepinte@gmail.com>
2013-08-03 12:16:12 +0200

    new implementation of Chain class

---
+++ src/Monad/Chain.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Monad;
+
+class Chain extends \MonadPHP\Monad
+{
+    public function __toString()
+    {
+        return (string) $this->extract();
+    }
+
+    public function __call($method, $args)
+    {
+        return $this->bind(
+            function ($obj) use ($method, $args) {
+                return $obj !== null ? call_user_func_array(array($obj, $method), $args) : null;
+            }
+        );
+    }
+
+    public function __get($name)
+    {
+        return $this->bind(
+            function ($obj) use ($name) {
+                return $obj !== null ? $obj->$name : null;
+            }
+        );
+    }
+
+    public function __set($name, $value)
+    {
+        return $this->bind(
+            function ($obj) use ($name, $value) {
+                if ($obj !== null) {
+                    $obj->$name = $value;
+                }
+            }
+        );
+    }
+}
